2c3e684ddd92cd05f55191084ac98611661b2344
valentimcanejo <valentim.canejo@hotmail.com>
2022-06-19 23:49:35 -0300

    feat: adicionando css global

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Header } from "./components/Header";
+import styles from "./App.module.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+      </div>
+    </div>
+  );
 }
 
 export default App;
